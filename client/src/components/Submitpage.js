9ebae9b07a658e5b257811a8628fd3a8528cbef9
imranyy <imranmatano6@gmail.com>
2022-04-01 12:10:04 -0700

    third commit

--- client/src/components/Submitpage.js
+++ client/src/components/Submitpage.js
@@ -1,5 +1,14 @@
+import {useEffect} from "react";
 import { Link } from "react-router-dom";
 const Submitpage=()=>{
+    useEffect(()=>{
+        const FetchReviews=async()=>{
+            const res=await fetch('http://localhost:5000/posts');
+            const data=await res.json();
+            console.log(data)
+        }
+        FetchReviews()
+    },[])
     return(
         <div style={{textAlign:'center'}}>
         <h2 style={{color:'green'}}>welcome</h2>
