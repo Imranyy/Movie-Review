c52b496c7a2971181e39f3715f4d2fa5dfd22fb1
imranyy <imranmatano6@gmail.com>
2022-04-08 14:08:35 +0300

    added new stuff

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -48,19 +48,42 @@ const Home=()=>{
       const close=document.querySelector('.login.open')
       close.classList.remove('open')
     }
+    //comment
+    const comment=()=>{
+       const comment=document.querySelector('.comment')
+       comment.classList.add('open')
+    }
+    //close comment
+    const closecomment=()=>{
+      const close=document.querySelector('.comment.open')
+      close.classList.remove('open')
+    }
+
     return(
         <div style={{textAlign:'center',alignItems:'center',zIndex:'0'}}>
       <Header title1="Reviews"/> <br/>
-        <Link to='/submitpage' style={{float:'right',marginRight:'12%', marginTop:'-90px'}}><button className="btn" style={{fontSize:'15px'}}>Add Review</button></Link>
-        <button className="btn" style={{marginRight:'1%', marginTop:'-90px'}} onClick={info}>info</button>
-        
-        <div className="login" onClick={closelogin}>
+        <Link to='/submitpage' style={{float:'right',marginRight:'19%', marginTop:'-90px'}}><button className="btn" style={{fontSize:'15px'}}>Add Review</button></Link>
+        <button className="btn" style={{marginRight:'10%', marginTop:'-90px'}} onClick={info}>info</button>
+        <button className="btn" style={{marginRight:'1%', marginTop:'-90px'}} onClick={comment}>Comment</button>
+        <div className="comment">
+          <div className="commentmodal">
+            <form>
+              <i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>email</i><input type='text' placeholder="enter email" required style={{height:'30px'}}/>
+              <i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>send</i><textarea placeholder="comment" required style={{height:'30px',width:'100%'}}/>
+              <button style={{margin:'5% auto'}}>Submit</button>
+            </form>
+            <buton onClick={closecomment} style={{float:'right',marginTop:'-8%', cursor:'pointer'}}> <i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>ads_click</i></buton>
+          </div>
+        </div>
+
+        <div className="login">
           <div className="loginmodal">
             <form>
               <i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>email</i><input type='text' placeholder="enter email" required style={{height:'30px'}}/>
               <i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>security</i><input type='password' placeholder="enter password" required style={{height:'30px'}}/>
               <button style={{margin:'5% auto'}}>Submit</button>
             </form>
+            <buton onClick={closelogin} style={{float:'right',marginTop:'-10%', cursor:'pointer'}}><i className="material-icons" style={{marginLeft:'-90%',fontSize:'200%'}}>ads_click</i></buton>
           </div>
         </div>
 
@@ -82,11 +105,11 @@ const Home=()=>{
               <h2 style={{borderBottom:'1px solid gray',width:'30%',margin:'0 auto'}}>About Me</h2>
               <p>I go by the name <em><strong>Ringside</strong></em>, the creator of this site. You can reach me by either the following:</p>
               <div style={{display:'flex',justifyContent:'space-around', marginTop:'-10px'}}>
-                <div><h4>Whatsapp:0754423664</h4><br/><a href="https://wa.me/+254754423664" target='_blank' rel="noopener"><i className="material-icons" style={{marginTop:'-100px',color:'green',cursor:'pointer'}}>whatsapp</i></a></div>
+                <div><h4>Whatsapp:0754423664</h4><br/><a href="https://wa.me/+254754423664"><i className="material-icons" style={{marginTop:'-100px',color:'green',cursor:'pointer'}}>whatsapp</i></a></div>
                 <div><h4>Email: imranmat245@gmail.com</h4><br/><i class="material-icons" style={{marginTop:'-100px', color:'blue'}}>email</i></div>
                 <div><h4>instagram: imrany</h4><br/><i className="material-icons" style={{marginTop:'-100px', color:'red'}}>chat</i></div>
               </div>
-              <button className="btn" onClick={login} style={{height:'30px', marginBottom:'10px',fontSize:'15px', padding:'0 auto',float:'left'}}>Staff Only<i className="material-icons" style={{fontSize:'12px'}}>not_interested</i></button>
+              <button className="btn" onClick={login} style={{height:'30px', marginBottom:'10px',fontSize:'15px', padding:'0 auto',float:'left'}}>Staff Only<i className="material-icons" style={{fontSize:'12px'}}>admin_panel_settings</i></button>
               <button className="btn" onClick={start} style={{height:'30px', marginBottom:'10px',fontSize:'15px', padding:'0 auto'}}>Leave</button>
             </div>
 
